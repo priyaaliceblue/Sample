5cbdffbe33f9906bdd952310b35af4bf1af97d2f
priyaaliceblue <priyapal@aliceblueindia.com>
2024-07-03 10:52:26 +0530

    Delete product-service directory

--- product-service/src/main/java/com/microservices/product_service/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.microservices.product_service.repository;
-
-import com.microservices.product_service.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product,Integer> {
-
-}
